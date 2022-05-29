fda6957124e8362b3b4e08e591aac291ceb2bdf8
ananttrivedi0709 <theananttrivedi@gmail.com>
2022-05-29 16:32:21 +0530

    2 Added fonts animations transitions

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,7 @@
 const boxImage = document.querySelector("main > img");
 const wonderButton = document.querySelector("main > button");
 const messageBox = document.querySelector("main > div");
+const linkButton = document.querySelector(".download-area > button");
 function openBox() {
   boxImage.src = "static/images/box-1-opened.png";
 }
@@ -26,6 +27,59 @@ function throwPopper() {
   });
 }
 
+function generateStars(selector) {
+  party.sparkles(selector, {
+    count: party.variation.range(1, 2),
+    size: party.variation.range(1.2, 1.8),
+  });
+}
+
+// function stopLinkBehaviour() {
+
+// }
+
+function activateRedirection(link) {
+  linkButton.addEventListener("click", () => {
+    window.open(link, "_blank");
+  });
+}
+
+function download(url) {
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = url.split("/").pop();
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+function makeDownloadButtonSmall() {
+  linkButton.classList.remove("show-full");
+  linkButton.classList.add("download-button-small");
+  linkButton.innerHTML = "Download Again!";
+}
+
+function createThanks(message) {
+  const p = document.createElement("p");
+  p.innerText = message;
+  messageBox.insertBefore(p, document.querySelector(".download-area"));
+}
+
+function removeDownloadText() {
+  document.querySelector(".download-button-text").remove();
+}
+
+function activateAssetDownloading(link, name) {
+  linkButton.addEventListener("click", () => {
+    // linkButton.setAttribute("href", link);
+    // linkButton.setAttribute("download", name);
+    download(link);
+    removeDownloadText();
+    createThanks("Hope you'll like it!");
+    makeDownloadButtonSmall();
+  });
+}
+
 wonderButton.addEventListener("click", () => {
   openBox();
   vanishWonderButton();
@@ -34,4 +88,12 @@ wonderButton.addEventListener("click", () => {
     bringBoxPositionDownAndAnimate();
     displayMessage();
   }, 2000);
+  //   activateRedirection("https://www.google.com");
+  activateAssetDownloading(
+    "https://drive.google.com/u/0/uc?id=1uk5bCziZpoiSVBx-Sj1OeVEa0iNcGobS&export=download",
+    "wonder-box-download.png"
+  );
+  setInterval(() => {
+    generateStars(boxImage);
+  }, 500);
 });
